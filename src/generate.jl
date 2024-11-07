7f25a2486a0798e3785faacf1f88d154f2ed3091
Markus Pirke <markus.pirke@fau.de>
2024-11-07 09:17:48 +0100

    Dict -> DiscreteNonParametric

--- src/generate.jl
+++ src/generate.jl
@@ -3,8 +3,8 @@
 
 This generates for a given n, λ and ρ the distributions of Minkowski functionals.
 The distribution for the area A is returned as a Binomial distribution from the Distributions.jl pacakge.
-The distributions for perimeter P and Euler characteristic are returned as dictionaries.
-Where the keys are P and χ and the values are the corresponding probabilities.
+The distributions for perimeter P and Euler characteristic are returned as a type DiscreteParametric from
+Distribution.jl, which is essentially a probability mass function.
 """
 function generate_distributions(n, λ, ρ)
     n_microstates = 2^(n^2)
@@ -12,18 +12,15 @@ function generate_distributions(n, λ, ρ)
     p = 1 - cdf(d_poisson, ρ-1)
 
     # lower and upper bounds for the Minkowski functionals in 2D
-    l_area = 0
-    u_area = n^2
     l_perimeter = 0
     u_perimeter = 4n^2
     l_euler = 2 - n^2
     u_euler = isodd(n) ? ((n+1)/2)^2 : (n/2)^2
-    N_euler = u_euler - l_euler
 
 
     d_area = Binomial(n^2, p)
-    d_perimeter = Dict(zip(l_perimeter:u_perimeter, [0.0 for _ in 1:(u_perimeter - l_perimeter)]))
-    d_euler = Dict(zip(l_euler:u_euler, [0.0 for _ in 1:(u_euler - l_euler + 1)]))
+    dict_perimeter = Dict(zip(l_perimeter:u_perimeter, [0.0 for _ in 1:(u_perimeter - l_perimeter)]))
+    dict_euler = Dict(zip(l_euler:u_euler, [0.0 for _ in 1:(u_euler - l_euler + 1)]))
 
 
     state = BitMatrix(undef, n, n)
@@ -39,9 +36,13 @@ function generate_distributions(n, λ, ρ)
         f = MinkowskiFunctional(state)
         p_f = pdf(d_area, f.A)
         normalization = binomial(n^2, Int(f.A))
-        d_perimeter[f.P] += p_f/normalization
-        d_euler[f.χ] += p_f/normalization
+        dict_perimeter[f.P] += p_f/normalization
+        dict_euler[f.χ] += p_f/normalization
     end
+
+    d_perimeter = DiscreteNonParametric(collect(keys(dict_perimeter)), collect(values(dict_perimeter)))
+    d_euler = DiscreteNonParametric(collect(keys(dict_euler)), collect(values(dict_euler)))
+
 
     return d_area, d_perimeter, d_euler
 end
--- test/generate.jl
+++ test/generate.jl
@@ -1,20 +1,22 @@
 using Test
 using MinkowskiFunctionals
 
+using Distributions
+
 @testset "generate" begin
     n = 2
     λ = 10
     ρ = 8
     d_A, d_P, d_χ = generate_distributions(n, λ, ρ)
-    @test sum(values(d_P)) ≈ 1
-    @test sum(values(d_χ)) ≈ 1
-    odd_P = [d_P[i] for i in 1:2:4n^2]
-    @test odd_P == [0.0 for _ in 1:2:4n^2] # perimeter cannot be odd
+    @test sum(probs(d_P)) ≈ 1
+    @test sum(probs(d_χ)) ≈ 1
+    odd_probs = probs(d_P)[isodd.(support(d_P))]
+    @test odd_probs == zeros(length(odd_probs)) # perimeter cannot be odd
 
     n = 3
-    d_A, d_P, d_χ = generate_distributions(3, λ, ρ)
-    @test sum(values(d_P)) ≈ 1
-    @test sum(values(d_χ)) ≈ 1
-    odd_P = [d_P[i] for i in 1:2:4n^2]
-    @test odd_P == [0.0 for _ in 1:2:4n^2] # perimeter cannot be odd
+    d_A, d_P, d_χ = generate_distributions(n, λ, ρ)
+    @test sum(probs(d_P)) ≈ 1
+    @test sum(probs(d_χ)) ≈ 1
+    odd_probs = probs(d_P)[isodd.(support(d_P))]
+    @test odd_probs == zeros(length(odd_probs)) # perimeter cannot be odd
 end
