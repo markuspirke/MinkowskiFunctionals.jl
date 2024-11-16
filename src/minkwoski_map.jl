9dadbcaec560c9f2b65302a65a355f2e73f08d26
Markus Pirke <markus.pirke@fau.de>
2024-11-16 15:19:05 +0100

    new method for MinkowskiMap

--- src/minkwoski_map.jl
+++ src/minkwoski_map.jl
@@ -30,6 +30,28 @@ struct MinkowskiMap
 
         return new(λ, ρs, L, Ds)
     end
+
+    function MinkowskiMap(x, h0_distributions)
+        m, n = size(x.pixels)
+        ρs = [s.ρ for s in h0_distributions]
+        λ = h0_distributions[1].λ
+        L = h0_distributions[1].n
+        l = floor(Int, L/2)
+        Ds = zeros(n - 2l, m - 2l)
+        for j in l+1:m-l
+            for i in l+1:n-l
+                deviation_strengths = zeros(length(ρs))
+                for (k, ρ) in enumerate(ρs)
+                    bw_map = BWMap(x, ρ)
+                    functional = MinkowskiFunctional(bw_map.pixels[i-l:i+l, j-l:j+l])
+                    deviation_strengths[k] = deviation_strength(h0_distributions[k], functional)
+                end
+                Ds[i-l, j-l] = maximum(deviation_strengths)
+            end
+        end
+
+        return new(λ, ρs, L, Ds)
+    end
 end
 
 
