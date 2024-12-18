41379e925164ecd3e4974c7629fbbec8046f3749
Markus Pirke <markus.pirke@fau.de>
2024-12-18 16:58:32 +0100

    changed MinkowskiMap

--- src/minkwoski_map.jl
+++ src/minkwoski_map.jl
@@ -8,95 +8,73 @@ struct MinkowskiMap
     ρ
     L
     pixels
+end
 
-    function MinkowskiMap(x, h0_distributions, F)
-        m, n = size(x.pixels)
-        ρs = [s.ρ for s in h0_distributions]
-        λ = h0_distributions[1].λ
-        L = h0_distributions[1].n
-        l = floor(Int, L/2)
-        Ds = zeros(n - 2l, m - 2l)
-        for j in l+1:m-l
-            for i in l+1:n-l
-                deviation_strengths = zeros(length(ρs))
-                for (k, ρ) in enumerate(ρs)
-                    bw_map = BWMap(x[i-l:i+l, j-l:j+l], ρ)
-                    functional = MinkowskiFunctional(bw_map.pixels)
-                    deviation_strengths[k] = deviation_strength(getfield(h0_distributions[k], F), getfield(functional, F))
-                end
-                Ds[i-l, j-l] = maximum(deviation_strengths)
-            end
-        end
-
-        return new(λ, ρs, L, Ds)
-    end
-
-    # function MinkowskiMap(x::CountsMap, h0_distributions::Vector{MinkowskiDistribution})
-    #     m, n = size(x.pixels)
-    #     ρs = [s.ρ for s in h0_distributions]
-    #     λ = h0_distributions[1].λ
-    #     L = h0_distributions[1].n
-    #     l = floor(Int, L/2)
-    #     Ds = zeros(n - 2l, m - 2l)
-    #     Threads.@threads for j in l+1:m-l
-    #         deviation_strengths = zeros(length(ρs))
-    #         for i in l+1:n-l
-    #             for (k, ρ) in enumerate(ρs)
-    #                 bw_map = BWMap(x, ρ)
-    #                 functional = MinkowskiFunctional(bw_map.pixels[i-l:i+l, j-l:j+l])
-    #                 deviation_strengths[k] = deviation_strength(h0_distributions[k], functional)
-    #             end
-    #             @inbounds Ds[i-l, j-l] = maximum(deviation_strengths)
-    #         end
-    #     end
-
-    #     return new(λ, ρs, L, Ds)
-    # end
-
-    function MinkowskiMap(x::CountsMap, h0_distributions::Vector{MinkowskiDistribution})
-        m, n = size(x.pixels)
-        ρs = [s.ρ for s in h0_distributions]
-        λ = h0_distributions[1].λ
-        L = h0_distributions[1].n
-        l = floor(Int, L/2)
-        Ds = zeros(n - 2l, m - 2l)
-        for j in l+1:m-l
+function MinkowskiMap(x, h0_distributions, F)
+    m, n = size(x.pixels)
+    ρs = [s.ρ for s in h0_distributions]
+    λ = h0_distributions[1].λ
+    L = h0_distributions[1].n
+    l = floor(Int, L/2)
+    Ds = zeros(n - 2l, m - 2l)
+    for j in l+1:m-l
+        for i in l+1:n-l
             deviation_strengths = zeros(length(ρs))
-            for i in l+1:n-l
-                for (k, ρ) in enumerate(ρs)
-                    bw_map = BWMap(x[i-l:i+l, j-l:j+l], ρ)
-                    functional = MinkowskiFunctional(bw_map.pixels)
-                    deviation_strengths[k] = h0_distributions[k].α[functional]
-                end
-                @inbounds Ds[i-l, j-l] = minimum(deviation_strengths)
+            for (k, ρ) in enumerate(ρs)
+                bw_map = BWMap(x[i-l:i+l, j-l:j+l], ρ)
+                functional = MinkowskiFunctional(bw_map.pixels)
+                deviation_strengths[k] = deviation_strength(getfield(h0_distributions[k], F), getfield(functional, F))
             end
+            Ds[i-l, j-l] = maximum(deviation_strengths)
         end
-
-        return new(λ, ρs, L, Ds)
     end
 
-    function MinkowskiMap(x::CountsMap, h0_distributions::Vector{MinkowskiDistribution}, fields)
-        m, n = size(x.pixels)
-        ρs = [s.ρ for s in h0_distributions]
-        λ = h0_distributions[1].λ
-        L = h0_distributions[1].n
-        l = floor(Int, L/2)
-        Ds = zeros(n - 2l, m - 2l)
-        for j in l+1:m-l
-            deviation_strengths = zeros(length(ρs))
-            for i in l+1:n-l
-                for (k, ρ) in enumerate(ρs)
-                    bw_map = BWMap(x[i-l:i+l, j-l:j+l], ρ)
-                    functional = MinkowskiFunctional(bw_map.pixels)
-                    reduced_functional = reduce_functional(functional, fields)
-                    deviation_strengths[k] = h0_distributions[k].α[reduced_functional]
-                end
-                @inbounds Ds[i-l, j-l] = minimum(deviation_strengths)
-            end
-        end
+    return MinkowskiMap(λ, ρs, L, Ds)
+end
 
-        return new(λ, ρs, L, Ds)
+function MinkowskiMap(x::CountsMap, h0_distributions::Vector{MinkowskiDistribution})
+    m, n = size(x.pixels)
+    ρs = [s.ρ for s in h0_distributions]
+    λ = h0_distributions[1].λ
+    L = h0_distributions[1].n
+    l = floor(Int, L/2)
+    Ds = zeros(n - 2l, m - 2l)
+    for j in l+1:m-l
+        deviation_strengths = zeros(length(ρs))
+        for i in l+1:n-l
+            for (k, ρ) in enumerate(ρs)
+                bw_map = BWMap(x[i-l:i+l, j-l:j+l], ρ)
+                functional = MinkowskiFunctional(bw_map.pixels)
+                deviation_strengths[k] = h0_distributions[k].α[functional]
+            end
+            @inbounds Ds[i-l, j-l] = minimum(deviation_strengths)
+        end
     end
+
+    return MinkowskiMap(λ, ρs, L, Ds)
+end
+
+function MinkowskiMap(x::CountsMap, h0_distributions::Vector{MinkowskiDistribution}, fields)
+    m, n = size(x.pixels)
+    ρs = [s.ρ for s in h0_distributions]
+    λ = h0_distributions[1].λ
+    L = h0_distributions[1].n
+    l = floor(Int, L/2)
+    Ds = zeros(n - 2l, m - 2l)
+    for j in l+1:m-l
+        deviation_strengths = zeros(length(ρs))
+        for i in l+1:n-l
+            for (k, ρ) in enumerate(ρs)
+                bw_map = BWMap(x[i-l:i+l, j-l:j+l], ρ)
+                functional = MinkowskiFunctional(bw_map.pixels)
+                reduced_functional = reduce_functional(functional, fields)
+                deviation_strengths[k] = h0_distributions[k].α[reduced_functional]
+            end
+            @inbounds Ds[i-l, j-l] = minimum(deviation_strengths)
+        end
+    end
+
+    return MinkowskiMap(λ, ρs, L, Ds)
 end
 
 
