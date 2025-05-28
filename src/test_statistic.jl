1c0e7d05c06112378e39d65230c586750d560d1c
Markus Pirke <markus.pirke@fau.de>
2025-05-28 15:38:45 +0200

    added eccdf stuff

--- src/test_statistic.jl
+++ src/test_statistic.jl
@@ -79,28 +79,45 @@ end
 struct ECCDF
     λ::Float64
     L::Int64
-    eccdf::Dict{Float64, Float64}
+    N::Int64
+    eccdf::SortedDict{Float64, Float64}
 end
 
-function ECCDF(λ::Float64, L::Int64, ecdf::T, xs) where {T <: ECDF}
+function ECCDF(λ::Float64, L::Int64, ecdf::T, N) where {T <: ECDF}
+    xs = vcat(range(minimum(ecdf), maximum(ecdf), N))
+    xs[end] -= 1e-8
     ys = 1 .- ecdf.(xs)
 
-    ECCDF(λ, L, Dict(x=>y for (x,y) in zip(xs, ys)))
+    ECCDF(λ, L, length(ecdf.sorted_values), SortedDict(Dict(x=>y for (x,y) in zip(xs, ys))))
+end
+
+function ECCDF(λ::Float64, L::Int64, N, n)
+    d = Dict(1 => AreaDistributionX(L^2, λ, 1))
+    dd = DefaultDict(0, d)
+    ts = [calc_ts!(dd, CountsMap(L, λ)) for _ in 1:N]
+    e_cdf = ecdf(ts)
+    eccdf = ECCDF(λ, L, e_cdf, n)
+
+    return eccdf
 end
 
 function (eccdf::ECCDF)(x::Float64)
-    ks = sort(collect(keys(eccdf.eccdf)))
-    closest_x = ks[searchsortednearest(ks, x)]
+    ks = collect(keys(eccdf.eccdf))
+    idx = searchsortedlast(ks, x)
+    idx == 0 && return 1.0
+    closest_x = ks[idx]
+
     return eccdf.eccdf[closest_x]
 end
 
 function compatibility(eccdf::ECCDF, dd::DefaultDict{Int64, AreaDistributionX, Int64}, x::Union{CountsMap, Matrix{Int64}})
-    ts = calc_ts!(dd, x)
+    ts = calc_ts(dd, x)
 
     return eccdf(ts)
 end
 
 function calc_ts!(dd::DefaultDict{Int64, AreaDistributionX, Int64}, x::Union{CountsMap, Matrix{Int64}})
+    b = dd[first(eachindex(dd))].λ
     L, _ = size(x)
     ρs = get_thresholds(x)
     for ρ in ρs
@@ -115,6 +132,46 @@ function calc_ts!(dd::DefaultDict{Int64, AreaDistributionX, Int64}, x::Union{Cou
     end
 
     return summed_ts
+end
+
+function calc_ts(dd::DefaultDict{Int64, AreaDistributionX, Int64}, x::Union{CountsMap, Matrix{Int64}})
+    ρs = get_thresholds(x)
+    summed_ts = 0.0
+    for ρ in ρs
+        summed_ts += -log10(compatibility(dd[ρ], x))
+    end
+
+    return summed_ts
+end
+
+function update_distributions!(dd::DefaultDict{Int64, AreaDistributionX, Int64}, x::CountsMap)
+    b = dd[first(eachindex(dd))].λ
+    n = dd[first(eachindex(dd))].n
+    ρs = get_thresholds(x)
+    for ρ in ρs
+        kys = keys(dd)
+        if !(ρ in kys)
+            dd[ρ] = AreaDistributionX(n, b, ρ)
+        end
+    end
+end
+
+function MinkowskiMap(x::CountsMap, mink_ds, eccdf::ECCDF)
+    λ = mink_ds[first(eachindex(mink_ds))].λ
+    m, n = size(x)
+    L = Int(sqrt(mink_ds[1].n))
+    l = floor(Int, L/2)
+    αs = zeros(n - 2l, m - 2l)
+    signs = zeros(n - 2l, m - 2l)
+    Threads.@threads for j in l+1:m-l
+        for i in l+1:n-l
+            local_counts = x[i-l:i+l, j-l:j+l]
+            pvalue = compatibility(eccdf, mink_ds, CountsMap(local_counts))
+            αs[i-l, j-l] = pvalue
+            signs[i-l, j-l] = mean(local_counts) > λ ? 1.0 : -1.0
+        end
+    end
+    MinkowskiMap(αs .* signs)
 end
 
 # function sample_pvalues(dd, b, N)
--- test/test_statistic.jl
+++ test/test_statistic.jl
@@ -1,6 +1,7 @@
 using MinkowskiFunctionals
 using Distributions
 using StatsBase
+using DataStructures
 using Test
 
 const SAMPLES_DIR = joinpath(@__DIR__, "samples")
@@ -34,7 +35,7 @@ const SAMPLES_DIR = joinpath(@__DIR__, "samples")
     λ = 10.0
     L = 15
     xs = 1:10
-    eccdf = ECCDF(λ, L, ecdf(xs), xs)
+    eccdf = ECCDF(λ, L, ecdf(xs), 10)
 
     @test 0.9 ≈ eccdf(1.0)
     @test 0.8 ≈ eccdf(2.0)
@@ -45,5 +46,21 @@ const SAMPLES_DIR = joinpath(@__DIR__, "samples")
     @test 0.3 ≈ eccdf(7.0)
     @test 0.2 ≈ eccdf(8.0)
     @test 0.1 ≈ eccdf(9.0)
-    @test 0.0 ≈ eccdf(10.0)
+    @test 0.1 ≈ eccdf(10.0)
+    @test 0.1 ≈ eccdf(11.0)
+
+    L = 5
+    λ = 10.0
+    d = Dict(1 => AreaDistributionX(L^2, λ, 1))
+    dd = DefaultDict(0, d)
+    ts = [calc_ts!(dd, CountsMap(L, λ)) for _ in 1:100]
+    e_cdf = ecdf(ts)
+    eccdf = ECCDF(λ, L, e_cdf, 100)
+    # @test 1 - e_cdf(1.0) ≈ eccdf(1.0) these will not work, as e_cdf has no equidistance bins, think about what to do here...
+    # @test 1 - e_cdf(20.0) ≈ eccdf(20.0)
+    counts_map = CountsMap(16, λ)
+    update_distributions!(dd, counts_map)
+    mink_map = MinkowskiMap(counts_map, dd, eccdf)
+    @test !isnan(maximum(p2σ(mink_map)))
+
 end
