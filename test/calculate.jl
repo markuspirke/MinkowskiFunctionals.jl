20dcc62b3b83249dc1870623b0beee1952e8a8da
Markus Pirke <markus.pirke@fau.de>
2025-07-15 17:09:05 +0200

    additional tests functionals

--- test/calculate.jl
+++ test/calculate.jl
@@ -23,4 +23,27 @@ using MinkowskiFunctionals
     @test M.P == 4
     @test M.χ == 1
 
+    image = [1 1; 1 1]
+    M = MinkowskiFunctional(image)
+    @test M.A == 4
+    @test M.P == 8
+    @test M.χ == 1
+
+    image = [1 0; 0 1]
+    M = MinkowskiFunctional(image)
+    @test M.A == 2
+    @test M.P == 8
+    @test M.χ == 1
+
+    image = [1 1; 0 1]
+    M = MinkowskiFunctional(image)
+    @test M.A == 3
+    @test M.P == 8
+    @test M.χ == 1
+
+    image = [1 0; 0 0]
+    M = MinkowskiFunctional(image)
+    @test M.A == 1
+    @test M.P == 4
+    @test M.χ == 1
 end
